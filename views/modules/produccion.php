a588997f78514ce9b9495adcc700856a32d3a1cb
Manuel Beltràn <manubega@yahoo.com>
2017-07-28 01:10:18 -0500

    Formulario Produccion
    Se valido la opción nuevo

--- views/modules/produccion.php
+++ views/modules/produccion.php
@@ -1,18 +1,37 @@
 <?php 
-
-
-
 session_start();
 if (!$_SESSION['root']) {
 	header('Location:../../index.php');
 }
 
  ?>
- 
 <div id = "prod1">
 	<h3>ORDEN DE PRODUCCION</h3>
+	<form method="post">
+		<select name = "opcion">
+		<option>Selecciòn</option>
+		<option value = "nuevo">Nuevo</option>
+		<option value = "editar">Editar</option>
+		<option value = "eliminar">Eliminar</option>
+	</select>
+	<input type="submit" name="boton" value ="enviar">
+	</form>
+	
 	<br>
-	<form method="POST" id="pro" onsubmit="return validaProduccion()">
+	<br>
+	<?php 
+	if (isset($_POST['opcion'])) {
+
+		$var = $_POST['opcion'];
+		
+	if (($var == 'nuevo')) {
+
+		?>
+		echo "NUEVO";
+				<form method="POST" id="pro" onsubmit="return validaProduccion()">
+	<input type="date" name="">
+	<br>
+	<br>
 	<input type="text" name="numeroOrden" placeholder="Orden No." required>
 	<br>
 	<br>
@@ -22,14 +41,24 @@ if (!$_SESSION['root']) {
 	<input type="text" name="nombreOrden" placeholder="compañia" required>
 	<br>
 	<br>
-	<input type="text" name="procesoOrden" placeholder="Proceso">
+	<input type="text" name="procesoOrden" placeholder="Proceso" required>
 	<br>
 	<br>
 
-	<input type="submit" value="Nuevo"  name="">
-	<input type="button" value="Editar" name="">
-	<input type="button" value="Eliminar" name="">
+	<input type="submit" value="Aceptar"  name="">
 </form>
+		<?php 
+			} 
+
+
+		if (($var == 'editar')) {
+		echo "EDITAR";	} 
+		if (($var == 'eliminar')) {
+		echo "ELIMINAR";	} 
+	}
+	
+		?>
+
 
 </div>
 
